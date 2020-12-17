0074be0e2c3dd45aa8ee3fbb5da3541752bcfbe5
TemptationWithin <75949800+TemptationWithin@users.noreply.github.com>
2020-12-17 16:47:21 +0300

    Update HomeWork_Lesson_3_Task_5.java

--- HomeWork_Lesson_3_Task_5.java
+++ HomeWork_Lesson_3_Task_5.java
@@ -21,47 +21,47 @@ public class HomeWork_Lesson_3_Task_5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите размерость матрицы А двумя числами:");   // ввод размерностей перемножаемых матриц
-        int x = scanner.nextInt();
-        int y = scanner.nextInt();
+        int aRows = scanner.nextInt();
+        int aColumns = scanner.nextInt();
         System.out.println("Введите размерость матрицы B двумя числами:");
-        int w = scanner.nextInt();
-        int z = scanner.nextInt();
-        if (y == w){                                                           // проверка возможности перемножения
-            int a[][] = new int[x][y];                                         // создание матриц
-            int b[][] = new int[w][z];
-            int result[][] = new int[x][z];
+        int bRows = scanner.nextInt();
+        int bColumns = scanner.nextInt();
+        if (aColumns == bRows){                                                           // проверка возможности перемножения
+            int a[][] = new int[aRows][aColumns];                                         // создание матриц
+            int b[][] = new int[bRows][bColumns];
+            int result[][] = new int[aRows][bColumns];
             System.out.println("Введите через Enter элементы матрицы А (построчно)");      // заполнение матрицы А
-            for (int i = 0; i < x; i++) {
-                for (int j = 0; j < y; j++) {
+            for (int i = 0; i < a.length; i++) {
+                for (int j = 0; j < a[0].length; j++) {
                     a[i][j] = scanner.nextInt();
                 }
             }
             System.out.println("Введите через Enter элементы матрицы B (построчно)");      // заполнение матрицы В
-            for (int i = 0; i < w; i++) {
-                for (int j = 0; j < z; j++) {
+            for (int i = 0; i < b.length; i++) {
+                for (int j = 0; j < b[0].length; j++) {
                     b[i][j] = scanner.nextInt();
                 }
             }
             scanner.close();                                                   // закрыли scanner
             System.out.print("Матрица А:");                                    // вывод матрицы А
-            for (int i = 0; i < x; i++) {                                      //Конструкции ("      ") при выводе используются для эстетичного вида матриц)
+            for (int i = 0; i < a.length; i++) {                               //Конструкции ("      ") при выводе используются для эстетичного вида матриц)
                 System.out.println("  ");
-                for (int j = 0; j < y; j++) {
+                for (int j = 0; j < a[0].length; j++) {
                     System.out.print(a[i][j] + "   ");
                 }
             }
             System.out.println(" ");                                            // вывод матрицы В
             System.out.println(" ");
             System.out.print("Матрица B:");
-            for (int i = 0; i < w; i++) {
+            for (int i = 0; i < b.length; i++) {
                 System.out.println("  ");
-                for (int j = 0; j < z; j++) {
+                for (int j = 0; j < b[0].length; j++) {
                     System.out.print(b[i][j] + "   ");
                 }
             }
-            for (int i = 0; i < x; i++) {                                       // вычисление перемножения
-                for (int j = 0; j < z; j++) {
-                    for (int n = 0; n < y; n++) {
+            for (int i = 0; i < a.length; i++) {                                // вычисление перемножения
+                for (int j = 0; j < b[0].length; j++) {
+                    for (int n = 0; n < a[0].length; n++) {
                             result[i][j] = result[i][j] + a[i][n] * b[n][j];
                         }
                     }
@@ -69,10 +69,10 @@ public class HomeWork_Lesson_3_Task_5 {
             System.out.println(" ");                                             // вывод результата
             System.out.println(" ");
             System.out.print("Результат перемножения AB(матрица С):");
-            for (int i = 0; i < x; i++) {
+            for (int i = 0; i < a.length; i++) {
                 System.out.println("  ");
-                for (int j = 0; j < z; j++) {
-                    System.out.print(result[i][j] + "   ");
+                for (int j = 0; j < b[0].length; j++) {
+                    System.out.print(result[i][j] + "  ");
                 }
             }
         } else {
